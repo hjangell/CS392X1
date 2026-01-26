b3ac52c2382942e88ad626ccfc4635832b8dd1af
hjangell <hjangell@bu.edu>
2026-01-26 14:17:25 -0500

    First attempt

--- assigns/01/Code/Payroll.java
+++ assigns/01/Code/Payroll.java
@@ -4,30 +4,84 @@ public class Payroll {
 
     public Payroll() {
 	/* your code */
+        maximum_size = INITIAL_MAXIMUM_SIZE;
+        current_size = 0;
+        people = new Employee[maximum_size];
+    }
+
+    public int size(){
+        // return current_size
+        return current_size;
+    }
+
+    public void print(){
+        // print the Employees currently on the Payroll, one per line
+        for(int i = 0; i < current_size; i++){
+            System.out.println(people[i].name);
+        }
     }
     
     public void add_employee(Employee newbie) {
 	/* your code */
+
+        // If Payroll is full, size will be doubled
+        if(current_size >= maximum_size){
+            maximum_size *= 2;
+
+            Employee[] temp_people = new Employee[maximum_size];
+            for(int i = 0; i < current_size; i++) temp_people[i] = people[i];
+            people = temp_people;
+        }
+
+        // Adding Employee on end of payroll
+        people[current_size] = newbie;
+        current_size++;
+
+
     }
 
     public void remove_employee(int i) throws EmployeeIndexException {
 	/* your code */
+        // Checking for no entry at position i
+        if(i >= current_size){
+            throw new EmployeeIndexException();
+        } 
+
+        // Replaces employee in people[i] with last employee
+        people[i] = people[current_size - 1];
+        current_size--;
+
     }
     
     public int find_employee(String name) throws EmployeeNotFoundException {
 	/* your code */
-    }
+        for(int i = 0; i < current_size; i++){
+            if(people[i].name == name) return i;
+        }
 
-    public void add_payroll(Payroll source) {
-	/* your code */
+        // If not found
+        throw new EmployeeNotFoundException();
+
     }
 
     public void copy_payroll(Payroll source) {
 	/* your code */
+        current_size = 0;
+        while(current_size < source.size()) add_employee(source.people[current_size]);
+        
     }
+
+    public void add_payroll(Payroll source) {
+	/* your code */
+        for(int i = 0; i < source.size(); i++){
+            add_employee(source.people[i]);
+        }
+    
+    }
+
+
 
     private Employee people[];
     private int maximum_size;
     private int current_size;
-    ...
 }
--- assigns/01/Code/TestPayroll.java
+++ assigns/01/Code/TestPayroll.java
@@ -1,4 +1,69 @@
 
 public class TestPayroll {
     /* your code */
+    public static void main(String[] args) {
+        // initialize testing payroll
+        Payroll testing = new Payroll(); 
+
+        // add people to payroll
+        Employee john = new Employee();
+        john.name = "john";
+        john.ID = 10;
+        john.salary = 10.0;
+        testing.add_employee(john);
+
+        // check Size
+        System.out.println(testing.size());
+
+        // print payroll
+        testing.print();
+
+        // add employee to payroll
+        Employee jim = new Employee();
+        jim.name = "jim";
+        jim.ID = 11;
+        jim.salary = 11.0;
+        testing.add_employee(jim);
+
+        // remove employee from payroll
+        try {
+            testing.remove_employee(0);
+        } catch( EmployeeIndexException ex){
+            System.out.println("Employee Index Exception");
+        }
+        
+
+        // find employee in payroll
+        try {
+            System.out.println("Index of jim: " + testing.find_employee("jim"));
+        } catch( EmployeeNotFoundException ex){
+            System.out.println("Employee Not Found Exception");
+        }
+        
+
+        // create second payroll
+        Payroll testing_two = new Payroll();
+        Employee jane = new Employee();
+        jane.name = "jane";
+        jane.ID = 12;
+        jane.salary = 12.0;
+        testing_two.add_employee(jane);
+
+        // add payroll together
+        testing.add_payroll(testing_two);
+
+        // print payroll
+        testing.print();
+        System.out.println(testing.size());
+
+        // copy payroll
+        testing.copy_payroll(testing_two);
+        System.out.println(testing.size());
+
+        // print payroll
+        testing.print();
+
+
+
+    }
 }
